1ed4a157d9c30d3a4e3e5dea4a26e91a2593a0a7
Your Name <you@example.com>
2025-06-18 16:27:17 +0600

    backup

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,22 +1,20 @@
 resource "azurerm_resource_group" "main" {
-  name     = "${local.project_name}-prod"
+  name     = "rg-${local.project_name}-${terraform.workspace}"
   location = var.location
   tags     = local.tags
 }
 
-
 resource "azurerm_kubernetes_cluster" "main" {
-  name                = "main-aks-1"
+  name                = "aks-${terraform.workspace}-${var.location}-001"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  dns_prefix          = "mainaks1"
+  dns_prefix          = replace(self.name, "-", "")
   tags                = local.tags
 
   default_node_pool {
     name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
-
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
   }
 
   identity {
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,3 +2,13 @@ variable "location" {
   description = "Location of all resources"
   type        = string
 }
+
+variable "node_count" {
+  description = "Number of worker nodes"
+  type        = number
+}
+
+variable "node_vm_size" {
+  description = "VM Size of worker nodes"
+  type        = string
+}
